35a66c3a0a245dad29e0695ad02a7f2524406830
jungheejung <heejung.jung@colorado.edu>
2020-06-16 14:50:46 -0400

    updated with proper child code

---
+++ scripts/s03_ridge-regression/c01_subject_jobs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import csv, subprocess
+
+parameter_file_full_path = "pbs_args.csv"
+
+# MODEL {0}
+# ALIGN {1}
+# STIM {2}
+# RUN {3}
+# HEMI {4}
+with open(parameter_file_full_path, "rb") as csvfile:
+    reader = csv.reader(csvfile)
+    for job in reader:
+        qsub_command = """mksub c02_life_encoding.pbs -F {0} {1} {2} {3} {4}""".format(*job)
+
+        print qsub_command # Uncomment this line when testing to view the qsub command
+
+        # Comment the following 3 lines when testing to prevent jobs from being submitted
+        exit_status = subprocess.call(qsub_command, shell=True)
+        if exit_status is 1:  # Check to make sure the job submitted
+            print "Job {0} failed to submit".format(qsub_command)
+print "Done submitting jobs!"
