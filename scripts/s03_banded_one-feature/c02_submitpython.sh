856fe4f707d32a281a04f99fdd311e0506f0151a
jungheejung <heejung.jung@colorado.edu>
2020-07-21 19:20:31 -0400

    code for averaging correlation coefficitns

---
+++ scripts/s03_banded_one-feature/c02_submitpython.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#PBS -N life_encode_submit_py
+#PBS -q default
+#PBS -l nodes=1:ppn=10
+#PBS -l walltime=08:00:00
+#PBS -m bea
+#PBS -A DBIC
+
+cd $PBS_O_WORKDIR
+
+module load python/2.7-Anaconda
+source /optnfs/common/miniconda3/etc/profile.d/conda.sh
+conda activate haxby_mvpc
+
+MODEL="visual"
+ALIGN="ws"
+STIM1="all"
+SUB=${1}
+RUN=${2}
+hemis=("lh" "rh")
+MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded_one-feature
+
+
+for HEMI in ${hemis[*]}; do
+python ${MAINDIR}/c03_banded_ridge.py  ${MODEL} ${ALIGN} ${STIM1} ${RUN} ${HEMI} ${SUB}
+echo "Running $CMD"
+done
+
