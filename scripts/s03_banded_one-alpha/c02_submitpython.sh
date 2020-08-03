2457a3b11acfd2e7bed0d6996bd30b6b59f733c1
jungheejung <heejung.jung@colorado.edu>
2020-08-03 15:02:27 -0400

    update code for one alpha

---
+++ scripts/s03_banded_one-alpha/c02_submitpython.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#PBS -N life_encode_cara_alpha
+#PBS -q default
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=20:00:00
+#PBS -m bea
+#PBS -A DBIC
+
+cd $PBS_O_WORKDIR
+
+module load python/2.7-Anaconda
+source /optnfs/common/miniconda3/etc/profile.d/conda.sh
+conda activate haxby_mvpc
+
+MODEL="visual"
+ALIGN="ws"
+STIM1="bg"
+STIM2="actions"
+SUB=${1}
+RUN=${2}
+hemis=("lh" "rh")
+MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded_one-alpha
+
+
+for HEMI in ${hemis[*]}; do
+python ${MAINDIR}/c03_banded_ridge.py  ${MODEL} ${ALIGN} ${STIM1} ${STIM2} ${RUN} ${HEMI} ${SUB}
+echo "Running $CMD"
+done
+
