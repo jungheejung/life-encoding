5092302b230452960f9367cd7fa5a3676a86d8a6
jungheejung <heejung.jung@colorado.edu>
2020-08-12 15:03:21 -0400

    add code

---
+++ scripts/s03_banded_cara-alpha_loro/c01_remainder.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+#PBS -N life_encode_cara_alpha
+#PBS -q default
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=24:00:00
+#PBS -m bea
+#PBS -A DBIC
+
+#mksub -F "sub-rid000001 2" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000001 - run 2"
+
+#mksub -F "sub-rid000005 0" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000005 - run 0"
+
+#mksub -F "sub-rid000005 2" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000005 - run 2"
+
+#mksub -F "sub-rid000006 2" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000006 - run 2"
+
+#mksub -F "sub-rid000006 3" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000006 - run 3"
+
+mksub -F "sub-rid000017 3" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+echo "Submitted job for sub-rid000017 - run 3"
+
+#mksub -F "sub-rid000027 3" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+echo "Submitted job for sub-rid000027 - run 3"
+
+#mksub -F "sub-rid000033 2" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000033 - run 2"
+
+#mksub -F "sub-rid000033 3" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000033 - run 3"
+
+#mksub -F "sub-rid000034 0" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000034 - run 0"
+
+mksub -F "sub-rid000034 3" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+echo "Submitted job for sub-rid000034 - run 3"
+
+#mksub -F "sub-rid000036 3" /dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded-ridge_cara-alpha/c02_submitpython.sh
+#echo "Submitted job for sub-rid000036 - run 3"
