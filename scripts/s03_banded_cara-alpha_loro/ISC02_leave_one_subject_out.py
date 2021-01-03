55c889417bbb9aa8f5eaed49ef98226e0667b39c
jungheejung <>
2021-01-02 19:31:14 -0500

    update indent

--- scripts/s03_banded_cara-alpha_loro/ISC02_leave_one_subject_out.py
+++ scripts/s03_banded_cara-alpha_loro/ISC02_leave_one_subject_out.py
@@ -73,7 +73,8 @@ for align in aligns:
                 subject_ids = np.arange(len(participants))
 
                 # stack
-                for subject in subject_ids:  # subject_ids+1):
+                for subject in subject_ids:
+                    # subject_ids+1):
                     # left_out_subject.shape (1200, 37476)
                     # other_subjects.shape (17, 1200, 37476)
                     # other_avg.shape (1200, 37476)
@@ -88,7 +89,7 @@ for align in aligns:
                         other_avg_voxel = other_avg[:, voxel]
                         isc = pearsonr(left_out_voxel, other_avg_voxel)[0]  # get r-value from pearsonr
                         isc_result[subject, voxel] = isc
-            	        triu_corrs = np.tanh(np.mean(np.nan_to_num(isc_result), axis=0))
+            	triu_corrs = np.tanh(np.mean(np.nan_to_num(isc_result), axis=0))
 		        med_wall_ind = np.where(cortical_vertices[hemi] == 0)[0]
     		    output = np.zeros((triu_corrs.shape[0] + med_wall_ind.shape[0]))
     		          output[cortical_vertices[hemi] == 1] = triu_corrs
