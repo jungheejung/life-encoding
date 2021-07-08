75286d6557030d812cdddd2c9809541889fa2b06
jungheejung <heejung.jung@colorado.edu>
2021-07-07 21:55:01 -0400

    update to sbatch

--- scripts/tikreg-pca/PCATIKREG_05ROI.sh
+++ scripts/tikreg-pca/PCATIKREG_05ROI.sh
@@ -1,39 +1,34 @@
 #!/bin/bash -l
-#PBS -N fe1aja
-#PBS -q default
-#PBS -l nodes=1:ppn=16
-#PBS -l walltime=12:00:00
-#PBS -A DBIC
-#PBS -l mem=50gb,vmem=60gb
 
-cd $PBS_O_WORKDIR
+#SBATCH --job-name=life_tikreg
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=12:00:00
+#SBATCH -o ./log_roi/tikreg_%A_%a.o
+#SBATCH -e ./log_roi/tikreg_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+#SBATCH --array=1-4%2
 
 module load python/2.7-Anaconda
 source /optnfs/common/miniconda3/etc/profile.d/conda.sh
 conda activate haxby_mvpc
 
-#MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/s03_banded_cara-alpha_loro
-#PBS -t 1:20
-# echo "PBSARRAY: " ${PBS_ARRAYID}
-# # INFILE=`awk "NR==${PBS_ARRAYID}" ${MAINDIR}/canonical_sublist.txt`
-# INFILE=`awk "NR==${PBS_ARRAYID}" ./node_test.txt`
-# SUB=$(echo $INFILE | cut -f1 -d,)
-# RUN=$(echo $INFILE | cut -f2 -d,)
-# HEMI=$(echo $INFILE | cut -f3 -d,)
-# NODE=$(echo $INFILE | cut -f4 -d,)
-# # SUB=$1
-# # RUN=$2
-# # HEMI=$3
-# # NODE=${PBS_ARRAYID}
+MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/tikreg-pca
+
+echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
+INFILE=`awk "NR==${SLURM_ARRAY_TASK_ID}" ./node_test.txt`
+SUB=$(echo $INFILE | cut -f1 -d,)
+RUN=$(echo $INFILE | cut -f2 -d,)
+HEMI=$(echo $INFILE | cut -f3 -d,)
+NODE=$(echo $INFILE | cut -f4 -d,)
 
 MODEL="visual"
 ALIGN="ws"
 STIM1="bg"
 STIM2="actions"
 STIM3="agents"
-NODE="vt"
-SUB="sub-rid000024"
-RUN=3
-HEMI="lh"
 
+hostname -s
 python ./PCATIKREG_05ROI_fullrange_10000.py ${MODEL} ${ALIGN} ${STIM1} ${STIM2} ${STIM3} ${RUN} ${HEMI} ${SUB} ${NODE}
