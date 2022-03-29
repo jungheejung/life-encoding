8262c2cd345c82c75130ab908654524e9b703819
jungheejung <heejung.jung@colorado.edu>
2022-03-29 11:37:12 -0400

    organize files

---
+++ scripts/archive/OLD_tikreg_alpha_loro/j01_ARRAYSUBMIT.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l 
+#PBS -N fe1aja
+#PBS -q default
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=12:00:00
+#PBS -A DBIC
+#PBS -t 1-3
+
+cd $PBS_O_WORKDIR
+
+module load python/2.7-Anaconda
+source /optnfs/common/miniconda3/etc/profile.d/conda.sh
+conda activate haxby_mvpc
+
+MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/tikreg_alpha_loro
+
+echo ${PBS_ARRAYID}
+echo "PBSARRAY"
+INFILE=`awk "NR==${PBS_ARRAYID}" ${MAINDIR}/sublist.txt`
+# INFILE=`awk "NR==${PBS_ARRAYID}" ${MAINDIR}/remainder.txt`
+SUB=$(echo $INFILE | cut -f1 -d,)
+RUN=$(echo $INFILE | cut -f2 -d,)
+HEMI=$(echo $INFILE | cut -f3 -d,)
+MODEL="visual"
+ALIGN="aa" # "ha_testsubj"  #"ws"
+STIM1="bg"
+STIM2="actions"
+STIM3="agents"
+
+python ${MAINDIR}/j02_banded_ridge.py  ${MODEL} ${ALIGN} ${STIM1} ${STIM2} ${STIM3} ${RUN} ${HEMI} ${SUB}
+
