15bd1f38b5897c38d32014735224c1afe65d6936
jungheejung <heejung.jung@colorado.edu>
2022-08-09 10:39:57 -0400

    ENH: submit list of job arrays via slurm_array.txt

--- scripts/himalaya/banded_ridge_slurm.sh
+++ scripts/himalaya/banded_ridge_slurm.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --job-name=life-himalaya
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=6:00:00
+#SBATCH --time=1:00:00
 #SBATCH -o ./log_roi/life_himalaya_%A_%a.o
 #SBATCH -e ./log_roi/life_himalaya_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
 
 # Vertices are split into chunks 0-39
-#SBATCH --array=1 
+#SBATCH --array=1-3 
 #-40
 
 conda activate himalaya
@@ -24,12 +24,16 @@ echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 # Subtract one to get python indexing
 ID=$((SLURM_ARRAY_TASK_ID-1))
 
+# Use slurm_array.sh file to set parameters
+ARRAY_FILE=${MAIN_DIR}/slurm_array.txt
+INFILE=`awk -F "," -v RS="\n" "NR==${ID}" ${ARRAY_FILE}`
+subject=$(echo $INFILE | cut -f1 -d,)
+hemisphere=$(echo $INFILE | cut -f2 -d,)
+test_run=$(echo $INFILE | cut -f3 -d,)
+roi=$(echo $INFILE | cut -f4 -d,)
+
 # Set command line arguments for banded_ridge.py
 alignment="ha_common" # ws, aa, ha_common, ha_test
-hemisphere="lh" # lh, rh
-test_run="2" # 1, 2, 3, 4
-test_subject="sub-rid000005"
 features="bg actions agents"
-roi=$ID
 
 python ${MAINDIR}/banded_ridge.py -a ${alignment} --hemisphere ${hemisphere} --test-run ${test_run} -s ${test_subject} -f ${features} --roi ${roi}
