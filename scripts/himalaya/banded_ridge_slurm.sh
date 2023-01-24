355d31e0d729d18dff851a0ca95881e8caac32b5
jungheejung <heejung.jung@colorado.edu>
2023-01-24 11:29:16 -0500

    STY: submit failed jobs; change log filename

--- scripts/himalaya/banded_ridge_slurm.sh
+++ scripts/himalaya/banded_ridge_slurm.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
 #SBATCH --time=01:00:00
-#SBATCH -o ./log_roi/life_himalaya_%A_%a.o
-#SBATCH -e ./log_roi/life_himalaya_%A_%a.e
+#SBATCH -o ./log_roi/ws_%A_%a.o
+#SBATCH -e ./log_roi/ws_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-
+#SBATCH --array=618,790,4889
 # Vertices are split into chunks 0-39
 ###SBATCH --array=1-5760%50 
 
@@ -33,7 +33,7 @@ test_run=$(echo $INFILE | cut -f3 -d,)
 roi=$(echo $INFILE | cut -f4 -d,)
 
 # Set command line arguments for banded_ridge.py
-alignment="ha_common" # ws, aa, ha_common, ha_test
+alignment="aa" # ws, aa, ha_common, ha_test
 features="bg actions agents"
 
 echo ${subject} ${hemisphere} ${test_run} ${roi}
