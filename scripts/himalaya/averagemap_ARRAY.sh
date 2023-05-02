023ca5e13d60952d9cd0d7e15f6b631037ba60d5
jungheejung <heejung.jung@colorado.edu>
2023-05-02 10:06:09 -0400

    DEV: average maps

---
+++ scripts/himalaya/averagemap_ARRAY.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=life-himalaya
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=01:00:00
+#SBATCH -o ./output/average_aa_%A_%a.o
+#SBATCH -e ./output/average_aa_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+#SBATCH --array=1-13%5 
+
+
+conda activate himalaya
+
+MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/himalaya
+echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
+
+# Subtract one to get python indexing
+ID=$((SLURM_ARRAY_TASK_ID-1))
+ALIGN="ws" #aa'
+
+
+python ${MAINDIR}/average_maps.py ${ID} ${ALIGN} 
