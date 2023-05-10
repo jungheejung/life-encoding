411438355f5befb75442d7fff9d574a74d53a1c2
jungheejung <>
2023-05-10 10:23:12 -0400

    BUG: rounding errors - we're explicitly removing a TR

--- scripts/motion_energy/moten_preproc.py
+++ scripts/motion_energy/moten_preproc.py
@@ -31,35 +31,35 @@ for run in [1,2,3,4]:
     TR = 2.5
     fmri_rate = 1/TR
 
-    number_of_samples = round(moten_log.shape[0] * fmri_rate / fps)
-    resample_scipy = signal.resample(moten_log, number_of_samples, axis = 0)
+    number_of_samples = np.ceil(moten_log.shape[0] * fmri_rate / fps)
+    resample_scipy = signal.resample(moten_log, int(number_of_samples), axis = 0)
     print(f"scipyresample shape: {resample_scipy.shape}")
     np.save(os.path.join(moten_dir, f'resample-scipy_run-{run}.npy'), resample_scipy)
-    
-    fig, axes = plt.subplots(ncols=1, figsize=(30, 10));    plt.plot(resample_scipy)
-    plt.title("downsampled via scipy.signal")
-    fig.savefig(os.path.join(moten_dir, f'resample-scipy_run-{run}.png'),dpi = 300)
+    # scipy plots
+    # fig, axes = plt.subplots(ncols=1, figsize=(30, 10));    plt.plot(resample_scipy)
+    # plt.title("downsampled via scipy.signal")
+    # fig.savefig(os.path.join(moten_dir, f'resample-scipy_run-{run}.png'),dpi = 300)
 
     # method 2: average over time window (TR) ___________________________________________
-    nframes = moten_log.shape[0]
-    nTRs = np.round(nframes/fps/TR)
-    downsamp = []
-    for i in np.arange(nTRs)*fps*TR:
-        downsamp.append(np.mean(moten_log[int(i):int(i+TR*fps)], axis=0))
-    resample_avg = np.vstack(downsamp)
-    np.save(os.path.join(moten_dir, f'resample-averageTR_run-{run}.npy'), resample_avg)
-    print(f"average resample shape: {np.array(resample_avg).shape}")
+    # nframes = moten_log.shape[0]
+    # nTRs = np.round(nframes/fps/TR)
+    # downsamp = []
+    # for i in np.arange(nTRs)*fps*TR:
+    #     downsamp.append(np.mean(moten_log[int(i):int(i+TR*fps)], axis=0))
+    # resample_avg = np.vstack(downsamp)
+    # np.save(os.path.join(moten_dir, f'resample-averageTR_run-{run}.npy'), resample_avg)
+    # print(f"average resample shape: {np.array(resample_avg).shape}")
 
-    fig, axes = plt.subplots(ncols = 1, figsize = (30, 10));    plt.plot(np.array(resample_avg))
-    plt.title("downsampled via averaging")
-    fig.savefig(os.path.join(moten_dir, f'resample-averageTR_run-{run}.png'), dpi = 300)
+    # fig, axes = plt.subplots(ncols = 1, figsize = (30, 10));    plt.plot(np.array(resample_avg))
+    # plt.title("downsampled via averaging")
+    # fig.savefig(os.path.join(moten_dir, f'resample-averageTR_run-{run}.png'), dpi = 300)
 
-    r = []
-    for i in np.arange(resample_scipy.shape[1]):
-        r.append(pearsonr(resample_scipy[:,i], resample_avg[:,i])[0])
-    print(f"run {run} correlation: {r}")
+    # r = []
+    # for i in np.arange(resample_scipy.shape[1]):
+    #     r.append(pearsonr(resample_scipy[:,i], resample_avg[:,i])[0])
+    # print(f"run {run} correlation: {r}")
 
-    # hstack _____________________________________________________________________________
+    # vstack _____________________________________________________________________________
     resample.append(resample_scipy)
 
     
@@ -68,8 +68,12 @@ for run in [1,2,3,4]:
 # %%
 resample_all = np.vstack(resample)
 model_durs = {1: 369, 2: 341, 3: 372, 4: 406}
-np.sum(model_durs.values()) == resample_all.shape[0]
+# assert np.sum(model_durs.values()) == resample_all.shape[0]
 
+# NOTE: The last run (run-4) has one more TR. 
+# We're explicitly removing a TR, due to rounding
+resample_remedy = resample_all[:-1]
+assert np.sum(list(model_durs.values())) == resample_remedy.shape[0]
 
 np.save(os.path.join(model_dir, 'visual_moten.npy'), resample_all)
 # %%
