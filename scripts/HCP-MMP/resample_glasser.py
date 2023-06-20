58670dfd8f94c4717d8dcf417c0ec9dcfc0bcbc9
jungheejung <heejung.jung@colorado.edu>
2023-06-20 19:13:44 -0400

    DEV: glasser roi

--- scripts/HCP-MMP/resample_glasser.py
+++ scripts/HCP-MMP/resample_glasser.py
@@ -7,22 +7,76 @@ from nilearn.plotting import plot_surf_stat_map
 import matplotlib.pyplot as plt
 from nilearn import datasets
 from matplotlib.colors import ListedColormap
+import numpy as np
 fsaverage = datasets.fetch_surf_fsaverage(mesh = 'fsaverage6')
 
-# %%
 
-# labels at each vertex
-# colors
-# names of labels
-lhatlas  = nib.freesurfer.io.read_annot('/Users/h/Dropbox (Dartmouth College)/projects_dropbox/life-encoding/scripts/HCP-MMP/lh.HCP_MMP1.fsaverage6.annot')
-lhatlas[0].shape
-len(lhatlas[2])
-# figure out name of the ROI from glasser atlas
-#
-# %%
-cmap = ListedColormap(lhatlas[1][:,:3]/255)
-plot_surf_stat_map(fsaverage['infl_left'], lhatlas[0],
-                    bg_map=fsaverage['sulc_left'],title=f'HCP-MMP1.0', 
-                    hemi='left', view='lateral', cmap = cmap)  # vmin=.05, vmax=.5,
+# %% Glasser brain labels and corresponding indices
+lh_labels, lh_ctab, lh_names= nib.freesurfer.io.read_annot('/Users/h/Documents/projects_local/life-encoding/scripts/HCP-MMP/lh.HCP_MMP1.fsaverage6.annot')
+rh_labels, rh_ctab, rh_names= nib.freesurfer.io.read_annot('/Users/h/Documents/projects_local/life-encoding/scripts/HCP-MMP/rh.HCP_MMP1.fsaverage6.annot')
+
+rois = {"PHG": [126,155,127],  # ParaHippocampal Gyrus
+        "V1": [1],             # Primary Visual
+        "EVC": [4,5],          # Early Visual Cortex (V2 Second Visual Area, V3 Third Visual Area)
+        "V4": [6],             # Fourth Visual Area
+        "SM": [8,9],           # primary motor, somato motor
+        "MT/MST": [2,23],      # Medial Superior Temporal Area, Middle Temporal Area
+        "RSC": [14],           # Retrosplenial
+        "LO": [20,21,159,156], # Lateral Occipital
+        "FFC": [18],           # Fusiform 
+        "PIT": [22],           # Posterior Inferior Temporal
+        "STV": [28],           # Superior Temporal Visual Area
+        # intraparietal
+        # premotor
+        }
+
 
 # %%
+def plot_roi(lh_roi_map, rh_roi_map, roi_name, cmap):
+    # align = os.path.basename(gii_dir)
+    figure, axes = plt.subplots(
+        1, 6, subplot_kw={'projection': '3d'}, figsize=(20, 30))
+
+    # double_c = np.vstack((cmap(np.linspace(0, 1, 100)), #
+                        # cmap(np.linspace(0, 1, 100))))
+    # cmap_convert = colors.ListedColormap(double_c)
+
+    plot_surf_stat_map(fsaverage['infl_left'], lh_roi_map,
+                       bg_map=fsaverage['sulc_left'],
+                       title=f'{roi_name} lateral lh', 
+                       hemi='left', view='lateral', cmap=cmap, axes=axes[0], colorbar=False)  # vmin=.05, vmax=.5,
+    plot_surf_stat_map(fsaverage['infl_right'], rh_roi_map,
+                       bg_map=fsaverage['sulc_right'],
+                       title=f'{roi_name} lateral rh', 
+                       hemi='right', view='lateral', cmap=cmap, axes=axes[1], colorbar=False)  # vmin=.05, vmax=.5,
+
+    plot_surf_stat_map(fsaverage['infl_left'], lh_roi_map,
+                       bg_map=fsaverage['sulc_left'],
+                       title=f'{roi_name} medial lh', 
+                       hemi='left', view='medial', cmap=cmap, axes=axes[2], colorbar=False)  # vmin=.05, vmax=.5,
+    plot_surf_stat_map(fsaverage['infl_right'], rh_roi_map,
+                       bg_map=fsaverage['sulc_right'],
+                       title=f'{roi_name} medial rh', 
+                       hemi='right', view='medial', cmap=cmap, axes=axes[3], colorbar=False)  # vmin=.05, vmax=.5,
+
+    plot_surf_stat_map(fsaverage['infl_left'], lh_roi_map,
+                       bg_map=fsaverage['sulc_left'],
+                       title=f'{roi_name} ventral lh', 
+                       hemi='left', view='ventral', cmap=cmap, axes=axes[4], colorbar=False)  # vmin=.05, vmax,=.5,
+    plot_surf_stat_map(fsaverage['infl_right'], rh_roi_map,
+                       bg_map=fsaverage['sulc_right'],
+                       title=f'{roi_name} ventral rh', 
+                       hemi='right', view='ventral', cmap=cmap, axes=axes[5], colorbar=False)  # vmin=.05, vmax=.5,
+
+    plt.show()
+
+# %%
+for roi_name in rois.keys():
+    lh_roi_map = np.full(lh_labels.shape, np.nan)
+    lh_roi_map[np.in1d(lh_labels, rois[roi_name])] = 1
+
+    rh_roi_map = np.full(rh_labels.shape, np.nan)
+    rh_roi_map[np.in1d(rh_labels, rois[roi_name])] = 1
+    plot_roi(lh_roi_map, rh_roi_map, roi_name, 'Reds')
+
+
