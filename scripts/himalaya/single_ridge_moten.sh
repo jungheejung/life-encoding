5be5266f13fc6a446d860783b3014d82c6337024
jungheejung <heejung.jung@colorado.edu>
2024-01-15 13:04:17 -0500

    DEV: single feature himalaya

--- scripts/himalaya/single_ridge_moten.sh
+++ scripts/himalaya/single_ridge_moten.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=life-himalaya
+#SBATCH --job-name=single_bg
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
 #SBATCH --time=00:30:00
-#SBATCH -o ./log_moten/hac_%A_%a.o
-#SBATCH -e ./log_moten/hac_%A_%a.e
+#SBATCH -o ./log_bgmoten/hac_%A_%a.o
+#SBATCH -e ./log_bgmoten/hac_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
 #SBATCH --array=1-50%50
