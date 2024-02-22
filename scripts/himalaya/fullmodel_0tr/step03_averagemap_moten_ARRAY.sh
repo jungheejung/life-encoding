ab7514cb3de0db0826d8289890f6eb46c227189b
jungheejung <heejung.jung@colorado.edu>
2024-02-22 18:50:08 -0500

    STY: rename files

---
+++ scripts/himalaya/fullmodel_0tr/step03_averagemap_moten_ARRAY.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=life-himalaya
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=01:00:00
+#SBATCH -o ./output/moten40_%A_%a.o
+#SBATCH -e ./output/moten40_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+#SBATCH --array=1-20%5 
+
+
+conda activate himalaya
+
+MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/himalaya
+echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
+
+# Subtract one to get python indexing
+ID=$((SLURM_ARRAY_TASK_ID-1))
+ALIGN="ha_common"
+#"/moten/ha_common_pca-40" #aa ws
+PCA=40
+ANALYSIS="0tr" # "pca"
+python ${MAINDIR}/step03_average_maps_moten.py ${ID} ${ALIGN} ${PCA} ${ANALYSIS}
