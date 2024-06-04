5327490730e3d68b0ccf3d70914c88810f1bedae
jungheejung <heejung.jung@colorado.edu>
2024-06-04 18:02:47 -0400

    DEV: slurm parameter

--- scripts/himalaya/singlemodel_nomoten/s01_single_ridge.sh
+++ scripts/himalaya/singlemodel_nomoten/s01_single_ridge.sh
@@ -9,14 +9,14 @@
 #SBATCH -e ./log_agents/hac_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-5750%50
+#SBATCH --array=4-5750%50
 # Vertices are split into chunks 0-39
 
 
 conda activate himalaya
 
 ### FIX THIS
-MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/himalaya
+MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 
 # Subtract one to get python indexing
@@ -26,7 +26,7 @@ ID=$((SLURM_ARRAY_TASK_ID-1))
 #ID=$((NUMBER_ARRAY[${SLURM_ARRAY_TASK_ID}]-1))
 echo ${ID}
 # Use slurm_array.sh file to set parameters
-ARRAY_FILE=${MAINDIR}/slurm_array.txt
+ARRAY_FILE="${MAINDIR}/scripts/himalaya/singlemodel_nomoten/slurm_array.txt"
 echo ${ARRAY_FILE}
 INFILE=`awk -F "," -v RS="\n" "NR==${ID}" ${ARRAY_FILE}`
 subject=$(echo $INFILE | cut -f1 -d,)
@@ -39,4 +39,4 @@ alignment="ha_common" # ws, aa, ha_common, ha_test
 features="agents" # "actions moten" "agents moten"
 
 echo ${subject} ${hemisphere} ${test_run} ${roi}
-python ${MAINDIR}/single_ridge.py -a ${alignment} --hemisphere ${hemisphere} --test-run ${test_run} -s ${subject} -f ${features} --roi ${roi}
+python ${MAINDIR}/scripts/himalaya/singlemodel_nomoten/single_ridge.py -a ${alignment} --hemisphere ${hemisphere} --test-run ${test_run} -s ${subject} -f ${features} --roi ${roi}
--- scripts/motion_energy/slurm.sh
+++ scripts/motion_energy/slurm.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -l
-
 #SBATCH --job-name=moten
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
-#SBATCH --mem-per-cpu=8gb
-#SBATCH --time=04:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=12gb
+#SBATCH --time=24:00:00
 #SBATCH -o ./log/moten_%A_%a.o
 #SBATCH -e ./log/moten_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-5
+#SBATCH --array=1-4
 
 
 conda activate pymoten
-
-MAINDIR=/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/motion_energy
-python ${MAINDIR}/moten_extract.py
+ID=$((SLURM_ARRAY_TASK_ID))
+MAINDIR="/dartfs/rc/lab/D/DBIC/DBIC/f0042x1/life-encoding/scripts/motion_energy"
+python ${MAINDIR}/step01_moten_extract_batch.py --run ${ID}
