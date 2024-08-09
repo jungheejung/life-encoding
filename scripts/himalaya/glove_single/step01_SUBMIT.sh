f3bff29b33b8b3fa218c6bc0bc1f01fcc3793a03
jungheejung <heejung.jung@colorado.edu>
2024-08-09 17:21:08 -0400

    DEV: update parameters

--- scripts/himalaya/glove_single/step01_SUBMIT.sh
+++ scripts/himalaya/glove_single/step01_SUBMIT.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=life-himalaya
+#SBATCH --job-name=life-single
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=05:30:00
+#SBATCH --time=03:30:00
 #SBATCH -o ./log_glove/hac_%A_%a.o
 #SBATCH -e ./log_glove/hac_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=388,389,390,391,396,409,410,411,412,422,5751,5752,5753,5754,5755,5756,5757,5758,5759,5760,5761
-##1-5750%50
+#SBATCH --array=1-8
+###5750%50
+
 # Vertices are split into chunks 0-39
 
 
@@ -37,7 +38,7 @@ roi=$(echo $INFILE | cut -f4 -d,)
 
 # Set command line arguments for banded_ridge.py
 alignment="ha_common" # ws, aa, ha_common, ha_test
-features="bg" # "actions" "agents" "moten"
+features="moten" # "bg" "actions" "agents" "moten"
 
 echo ${subject} ${hemisphere} ${test_run} ${roi}
 python ${MAINDIR}/scripts/himalaya/glove_single/step01_banded_ridge_moten_0tr_glove.py \
