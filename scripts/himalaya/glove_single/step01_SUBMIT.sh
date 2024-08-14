bf26eb74a39e2b2f0499fd7ed4be95616cb3c183
jungheejung <heejung.jung@colorado.edu>
2024-08-14 14:31:05 -0400

    DOC: slurm parameters change for job submission

--- scripts/himalaya/glove_single/step01_SUBMIT.sh
+++ scripts/himalaya/glove_single/step01_SUBMIT.sh
@@ -4,13 +4,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=03:30:00
-#SBATCH -o ./log_glove/hac_%A_%a.o
-#SBATCH -e ./log_glove/hac_%A_%a.e
+#SBATCH --time=00:20:00
+#SBATCH -o ./log_bgmoten/hac_%A_%a.o
+#SBATCH -e ./log_bgmoten/hac_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-8
-###5750%50
+#SBATCH --array=1-5760%100
 
 # Vertices are split into chunks 0-39
 
@@ -38,7 +37,7 @@ roi=$(echo $INFILE | cut -f4 -d,)
 
 # Set command line arguments for banded_ridge.py
 alignment="ha_common" # ws, aa, ha_common, ha_test
-features="moten" # "bg" "actions" "agents" "moten"
+features="bg moten" # "bg" "actions" "agents" "moten"
 
 echo ${subject} ${hemisphere} ${test_run} ${roi}
 python ${MAINDIR}/scripts/himalaya/glove_single/step01_banded_ridge_moten_0tr_glove.py \
